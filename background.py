abe504b98f2ff9784d9e53d8843969c555c79d8b
hakob-petro <55916687+hakob-petro@users.noreply.github.com>
2020-07-31 10:08:33 +0400

    Update background.py

--- background.py
+++ background.py
@@ -6,13 +6,15 @@ import random
 from pygame import *
 
 
-class background(pygame.sprite.Sprite):
+class Background(pygame.sprite.Sprite):
     def __init__(self, image_file, location):
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.image.load(image_file)
         self.rect = self.image.get_rect()
         self.rect.left, self.rect.top = location
+        self.location = location
         
     def draw(self, screen):
-            screen.blit(self.image, [0, 0])
+            screen.blit(self.image, self.location)
             
+
