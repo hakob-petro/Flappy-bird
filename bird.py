ef2858457908569b311f3c4e491a4149dde4cfb4
hakob-petro <55916687+hakob-petro@users.noreply.github.com>
2020-08-25 19:38:19 +0400

    Update bird.py

--- bird.py
+++ bird.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+
 from pygame.sprite import Sprite
 from pygame import Surface
 from pygame.time import Clock
 from pyganim import PygAnimation
 #from pygame.image import load
+
 
 SPEED = 0
 JUMP = 10
@@ -24,9 +26,9 @@ ANIMATION = ["bird/skeleton-animation_00.png",
              "bird/skeleton-animation_09.png",
              "bird/skeleton-animation_10.png"]
 
-class Bird(pygame.sprite.Sprite):
+class Bird(Sprite):
     def __init__(self, xpos, ypos, image):
-        pygame.sprite.Sprite.__init__(self)
+        Sprite.__init__(self)
         self.x = xpos
         self.y = ypos
         self.yvel = 0
@@ -35,7 +37,7 @@ class Bird(pygame.sprite.Sprite):
         self.end = False
         self.score = 0
         self.image = Surface((53, 30))
-        #self.image = pygame.image.load(image)
+        #self.image = load(image)
         self.rect = self.image.get_rect()
         self.rect.x = xpos
         self.rect.y = ypos
@@ -44,7 +46,7 @@ class Bird(pygame.sprite.Sprite):
         animations = []
         for anim in  ANIMATION:
             animations.append((anim, ANIMATION_DELAY))
-        self.anim = pyganim.PygAnimation(animations)
+        self.anim = PygAnimation(animations)
         self.anim.play()
         
 
@@ -52,7 +54,7 @@ class Bird(pygame.sprite.Sprite):
         screen.blit(self.image, (self.x, self.y))
 
     def update(self):
-        timer = pygame.time.Clock()
+        timer = Clock()
         self.image.fill(COLOR)
         self.anim.blit(self.image, (0, 0))
         
